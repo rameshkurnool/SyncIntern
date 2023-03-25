d359ab6ec3074620e6dcec07e3ebf8e5ebfaf8d5
rameshkurnool <128351836+rameshkurnool@users.noreply.github.com>
2023-03-25 15:48:37 +0530

    Update OTP verify.py

--- OTP verify.py
+++ OTP verify.py
@@ -1,39 +1,33 @@
 import random
-
-otp = random.randint(100000, 999999)
-print("Your OTP is:", otp)
-
-# Storing the OTP in a variable
-otp_input = input("Enter the OTP you received: ")
-
-if otp == int(otp_input):
-    print("Verification successful!")
-else:
-    print("Verification failed.")
-
-import random
 import smtplib
 
-fromaddr = "rameshkurnool777@gmail.com"
-msg = "Your OTP is: "
+# generate OTP
+otp = random.randint(1000, 9999)
 
-otp = random.randint(100000, 999999)
+# email information
+sender_email = 'rameshkurnool777@gmail.com'
+sender_password = 'ypchhblcskypzgam'
+receiver_email = 'rameshkurnool20@gmail.com'
+message = f'Your OTP is {otp}'
 
-# Send the OTP to the user's email
-toaddr = input("rameshkurnool20@gmail.com")
-msg += str(otp)
+# SMTP server information
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
+# connect to SMTP server
+server = smtplib.SMTP(smtp_server, smtp_port)
 server.starttls()
-server.login(fromaddr, 'sadhguru@777')
-server.sendmail(fromaddr, toaddr, msg)
-server.quit()
+server.login(sender_email, sender_password)
 
-# Request user input for the OTP
-otp_input = input("Enter the otp you recieved: ")
+# send email with OTP
+server.sendmail(sender_email, receiver_email, message)
+print('OTP sent successfully')
 
-# Verify the OTP
-if otp == int(otp_input):
-    print("Verification successful!")
+# prompt user to enter OTP
+user_otp = input('Enter the OTP sent to your email: ')
+
+# verify OTP
+if int(user_otp) == otp:
+    print('OTP verified successfully')
 else:
-    print("Verification failed.")
+    print('OTP verification failed')
